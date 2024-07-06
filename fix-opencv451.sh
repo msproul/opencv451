819f3728ebdadfa9169f3ac980880d213780935a
Mark Sproul <msproul@skychariot.com>
2024-07-06 13:33:18 -0400

    Added more info to the fix script

--- fix-opencv451.sh
+++ fix-opencv451.sh
@@ -7,6 +7,8 @@ BAD_FILE="opencv/opencv-4.5.1/modules/gapi/test/gapi_async_test.cpp"
 echo "Fixing $BAD_FILE"
 
 ISFIXEDCOUNT=`grep "#include" $BAD_FILE | grep thread | wc -l`
+
+
 echo $ISFIXEDCOUNT
 
 if [ $ISFIXEDCOUNT -gt 0 ]
@@ -14,13 +16,21 @@ then
 	echo "File is already fixed"
 	grep "#include" $BAD_FILE | grep thread
 else
+	echo "The file has not been fixed, working on that now"
 
-	exit
 
 	echo "#include <thread>"  > $TEMP_FILE
 
 	cat $BAD_FILE >> $TEMP_FILE
 	mv $BAD_FILE  bad_gapi_async_test.cpp
 	mv $TEMP_FILE $BAD_FILE
+
+	ISFIXEDCOUNT=`grep "#include" $BAD_FILE | grep thread | wc -l`
+	if [ $ISFIXEDCOUNT -gt 0 ]
+	then
+		echo "File fixed succesuflly"
+	else
+		echo "The file has not been fixed, Somthing is wrong"
+	fi
 fi
 
